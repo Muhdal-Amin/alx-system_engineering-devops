49d58d832e8a343d06f963ee85639cc2cb7a00e6
Muhdal-Amin <adalemo2000@gmail.com>
2023-11-24 14:20:04 -0800

    Creating a file in /tmp using Puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
 # Creating a file in /tmp using puppet
-
 file { '/tmp/school':
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love puppet'
+  content => 'I love Puppet'
 }
